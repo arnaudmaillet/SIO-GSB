559866dba2cde5a0aa3e547fe6ad4dd9571b7cd2
arnaudMaillet <arnaud.maillet@saint-remi.net>
2021-05-05 23:24:23 +0200

    Done : Frm Echantillons

--- GSB/GSB/FrmEchantillon.Designer.cs
+++ GSB/GSB/FrmEchantillon.Designer.cs
@@ -31,8 +31,12 @@ namespace GSB
         {
             this.dgvEchantillons = new System.Windows.Forms.DataGridView();
             this.panel2 = new System.Windows.Forms.Panel();
+            this.dgvVisites = new System.Windows.Forms.DataGridView();
+            this.panel3 = new System.Windows.Forms.Panel();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEchantillons)).BeginInit();
             this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisites)).BeginInit();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblTitre
@@ -45,35 +49,57 @@ namespace GSB
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.dgvEchantillons.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvEchantillons.Location = new System.Drawing.Point(52, 3);
+            this.dgvEchantillons.Location = new System.Drawing.Point(3, 3);
             this.dgvEchantillons.Name = "dgvEchantillons";
             this.dgvEchantillons.RowHeadersWidth = 51;
             this.dgvEchantillons.RowTemplate.Height = 24;
-            this.dgvEchantillons.Size = new System.Drawing.Size(701, 547);
+            this.dgvEchantillons.Size = new System.Drawing.Size(527, 570);
             this.dgvEchantillons.TabIndex = 13;
+            this.dgvEchantillons.SelectionChanged += new System.EventHandler(this.dgvEchantillons_SelectionChanged);
             // 
             // panel2
             // 
             this.panel2.Controls.Add(this.dgvEchantillons);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 92);
+            this.panel2.Location = new System.Drawing.Point(8, 128);
             this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1450, 641);
+            this.panel2.Size = new System.Drawing.Size(530, 598);
             this.panel2.TabIndex = 14;
+            // 
+            // dgvVisites
+            // 
+            this.dgvVisites.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVisites.Location = new System.Drawing.Point(3, 3);
+            this.dgvVisites.Name = "dgvVisites";
+            this.dgvVisites.RowHeadersWidth = 51;
+            this.dgvVisites.RowTemplate.Height = 24;
+            this.dgvVisites.Size = new System.Drawing.Size(894, 571);
+            this.dgvVisites.TabIndex = 14;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.dgvVisites);
+            this.panel3.Location = new System.Drawing.Point(541, 127);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(909, 599);
+            this.panel3.TabIndex = 15;
             // 
             // FrmEchantillon
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1450, 801);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
             this.Name = "FrmEchantillon";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FrmEchantillon_Load);
             this.Controls.SetChildIndex(this.lblTitre, 0);
             this.Controls.SetChildIndex(this.panel2, 0);
+            this.Controls.SetChildIndex(this.panel3, 0);
             ((System.ComponentModel.ISupportInitialize)(this.dgvEchantillons)).EndInit();
             this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisites)).EndInit();
+            this.panel3.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,5 +109,7 @@ namespace GSB
 
         private System.Windows.Forms.DataGridView dgvEchantillons;
         private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgvVisites;
+        private System.Windows.Forms.Panel panel3;
     }
 }
--- GSB/GSB/FrmEchantillon.cs
+++ GSB/GSB/FrmEchantillon.cs
@@ -22,8 +22,14 @@ namespace GSB
         private void FrmEchantillon_Load(object sender, EventArgs e)
         {
             parametrerComposant();
-            parametrerDgv(dgvEchantillons);
-            remplirDgvEchantillon();
+            parametrerDgvEchantillons(dgvEchantillons);
+            parametrerDgvVisites(dgvVisites);
+            remplirDgvEchantillons();
+        }
+
+        private void dgvEchantillons_SelectionChanged(object sender, EventArgs e)
+        {
+            remplirDgvVisites();
         }
         #endregion
 
@@ -33,7 +39,7 @@ namespace GSB
             this.lblTitre.Text = "Liste des Echantillons";
         }
 
-        private void parametrerDgv(DataGridView unDgv)
+        private void parametrerDgvEchantillons(DataGridView unDgv)
         {
             #region paramètrage concernant le datagridview dans son ensemble
 
@@ -143,10 +149,10 @@ namespace GSB
             #region paramètrage des colonnes
 
             // Largeur : à contrôler avec la largeur des colonnes si elle est définie
-            unDgv.Width = 800;
+            unDgv.Width = 400;
 
             // Nombre de colonne sans compter les colonnes ajoutées par la méthode Add
-            unDgv.ColumnCount = 6;
+            unDgv.ColumnCount = 3;
 
             // faut-il ajuster automatiquement la taille des colonnes à leur contenu (commenter la ligne si non)
             // unDgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
@@ -159,29 +165,14 @@ namespace GSB
 
             unDgv.Columns[1].HeaderText = "Médicament";
             unDgv.Columns[1].Name = "Medicament";
-            unDgv.Columns[1].Width = 150;
+            unDgv.Columns[1].Width = 250;
             unDgv.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
 
             unDgv.Columns[2].HeaderText = "Total";
             unDgv.Columns[2].Name = "Total";
-            unDgv.Columns[2].Width = 50;
+            unDgv.Columns[2].Width = 150;
             unDgv.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
             unDgv.Columns[2].HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
-
-            unDgv.Columns[3].HeaderText = "Distribué le";
-            unDgv.Columns[3].Name = "Date";
-            unDgv.Columns[3].Width = 150;
-            unDgv.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-
-            unDgv.Columns[4].HeaderText = "Chez";
-            unDgv.Columns[4].Name = "Praticien";
-            unDgv.Columns[4].Width = 100;
-            unDgv.Columns[4].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
-
-            unDgv.Columns[5].HeaderText = "Quantité";
-            unDgv.Columns[5].Name = "Qte";
-            unDgv.Columns[5].Width = 100;
-            unDgv.Columns[5].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
 
             // faut-il désactiver le tri sur toutes les colonnes ? (commenter les lignes si non)
             for (int i = 0; i < unDgv.ColumnCount; i++)
@@ -194,20 +185,168 @@ namespace GSB
 
         }
 
-        private void remplirDgvEchantillon()
+        private void parametrerDgvVisites(DataGridView unDgv)
+        {
+            #region paramètrage concernant le datagridview dans son ensemble
+
+            // style de bordure
+            unDgv.BorderStyle = BorderStyle.FixedSingle;
+
+            // couleur de fond 
+            unDgv.BackgroundColor = Color.White;
+
+            // couleur de texte  
+            unDgv.ForeColor = Color.Black;
+
+            // police de caractères par défaut
+            unDgv.DefaultCellStyle.Font = new Font("Georgia", 11);
+
+            // mode de sélection dans le composant : FullRowSelect, CellSelect ...
+            unDgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            // sélection multiple 
+            unDgv.MultiSelect = false;
+
+            // l'utilisateur peut-il ajouter ou supprimer des lignes ?
+            unDgv.AllowUserToDeleteRows = false;
+            unDgv.AllowUserToAddRows = false;
+
+            // L'utilisateur peut-il modifier le contenu des cellules ou est-elle réservée à la programmation ?
+            unDgv.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+            // l'utilisateur peut-il redimensionner les colonnes et les lignes ?
+            unDgv.AllowUserToResizeColumns = false;
+            unDgv.AllowUserToResizeRows = false;
+
+            // l'utilisateur peut-il modifier l'ordre des colonnes ?
+            unDgv.AllowUserToOrderColumns = false;
+
+            // le composant accepte t'il le 'déposer' dans un Glisser - Déposer ?
+            unDgv.AllowDrop = false;
+
+            #endregion
+
+            #region paramètrage concernant la ligne d'entête (les entêtes de chaque colonnes)
+
+            // visibilité
+            unDgv.ColumnHeadersVisible = true;
+
+            // bordure
+            unDgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+            // style  [à adapter] (ici : noir sur fond transparent sans mise en évidence de la sélection)
+            unDgv.EnableHeadersVisualStyles = false;
+            DataGridViewCellStyle style = unDgv.ColumnHeadersDefaultCellStyle;
+            style.BackColor = Color.Gray;
+            style.ForeColor = Color.Black;
+            style.SelectionBackColor = Color.WhiteSmoke;    // même couleur que backColor pour ne pas mettre en évidence la colonne sélectionnée
+            style.SelectionForeColor = Color.Black;
+            style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            style.Font = new Font("Georgia", 12, FontStyle.Bold);
+
+
+            // hauteur 
+            unDgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            unDgv.ColumnHeadersHeight = 40;
+
+            #endregion
+
+            #region paramètrage concernant l'entête de ligne (la colonne d'entête ou le sélecteur)
+
+            // visible 
+            unDgv.RowHeadersVisible = false;
+
+            // style de bordure  
+            unDgv.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+
+            #endregion
+
+            #region paramètrage au niveau des lignes
+
+            // Hauteur 
+            unDgv.RowTemplate.Height = 30;
+
+            #endregion
+
+            #region paramètrage au niveau des cellules
+
+            // style de bordure 
+            unDgv.CellBorderStyle = DataGridViewCellBorderStyle.None;
+
+            // couleur de fond, ne pas utiliser transparent car la couleur de la ligne sélectionnée restera après sélection
+            unDgv.RowsDefaultCellStyle.BackColor = Color.White;
+
+            // Couleur alternative appliquée une ligne sur deux
+            // unDgv.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(255, 238, 238, 238);
+
+            #endregion
+
+            #region paramètrage au niveau de la zone sélectionnée
+
+            // couleur de fond mettre la même que les cellules si on ne veut pas mettre la zone en évidence 
+            unDgv.RowsDefaultCellStyle.SelectionBackColor = System.Drawing.Color.Lavender;
+
+            // couleur du texte
+            unDgv.RowsDefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+
+            #endregion
+
+            #region paramètrage des colonnes
+
+            // Largeur : à contrôler avec la largeur des colonnes si elle est définie
+            unDgv.Width = 600;
+
+            // Nombre de colonne sans compter les colonnes ajoutées par la méthode Add
+            unDgv.ColumnCount = 4;
+
+            // faut-il ajuster automatiquement la taille des colonnes à leur contenu (commenter la ligne si non)
+            // unDgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.AllCells;
+
+            // faut-il ajuster automatiquement la taille des colonnes par un ajustement proportionnel à la largeur totale (commenter la ligne si non)
+            unDgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            // description de chaque colonne  [partie à personnaliser] : visibilité, largeur, alignement cellule et entête si ellene correspond pas à la valeur par défaut
+            unDgv.Columns[0].Visible = false;
+
+            unDgv.Columns[1].HeaderText = "Distribué le";
+            unDgv.Columns[1].Name = "Date";
+            unDgv.Columns[1].Width = 150;
+            unDgv.Columns[1].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            
+            unDgv.Columns[2].HeaderText = "Chez";
+            unDgv.Columns[2].Name = "Praticien";
+            unDgv.Columns[2].Width = 100;
+            unDgv.Columns[2].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            
+            unDgv.Columns[3].HeaderText = "Quantité";
+            unDgv.Columns[3].Name = "Qte";
+            unDgv.Columns[3].Width = 100;
+            unDgv.Columns[3].DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+
+
+            // faut-il désactiver le tri sur toutes les colonnes ? (commenter les lignes si non)
+            for (int i = 0; i < unDgv.ColumnCount; i++)
+                unDgv.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+
+            #endregion
+
+            // Dimensionner la hauteur du DataGridview en fonction du nombre de lignes
+            unDgv.Height = Globale.LesMedicaments.Count * (unDgv.RowTemplate.Height) + unDgv.ColumnHeadersHeight + 10;
+
+        }
+
+        private void remplirDgvEchantillons()
         {
             // Dictionnaire lesMedicaments = [{Medicament, total} , ...] contenant tout les médicaments (clé unique) + le total par médicaments
             SortedDictionary<Medicament, int> lesMedicaments = new SortedDictionary<Medicament, int>();
-            SortedDictionary<Visite, List<Medicament>> lesVisites = new SortedDictionary<Visite, List<Medicament>>();
-            List<Medicament> array = new List<Medicament>();
 
             foreach (Visite uneVisite in Globale.LeVisiteur.getLesVisitesCloses())
             {
                 // Dictionnaire lesEchantillons contenant les echantillons pour une visite close
                 SortedDictionary<Medicament, int> lesEchantillons = uneVisite.getLesEchantillons();
                 
-                lesVisites.Add(uneVisite, array);
-
+                //lesVisites.Add(uneVisite, array);
                 foreach (KeyValuePair<Medicament, int> unEchantillon in lesEchantillons)
                 {
                     // Si unEchantillon est déjà présent dans lesMedicaments => on supprime la ligne de l'echantillon et on rajoute la clé en ajoutant la valeur (qte) avec la précendante sinon on ajoute la clé + valeur dans lesMedicaments
@@ -219,19 +358,9 @@ namespace GSB
                     } else
                     {
                         lesMedicaments.Add(unEchantillon.Key, unEchantillon.Value);
-                        array.Add(unEchantillon.Key);
                     }
                 }
             }
-
-            
-
-            //foreach (KeyValuePair<Medicament, int> unMedicament in lesMedicaments)
-            //{
-            //    
-            //
-            //    Visite lastVisite = lesMedicaments.ContainsKey(Globale.LeVisiteur.getLesVisites())
-            //}
 
             foreach(KeyValuePair<Medicament, int> unMedicament in lesMedicaments)
             { 
@@ -239,11 +368,43 @@ namespace GSB
                     unMedicament,
                     unMedicament.Key,
                     unMedicament.Value
-                    //currentVisite.DateEtHeure,
-                    //currentVisite.LePraticien.NomPrenom
                 );
             }
             #endregion
         }
+
+        private void remplirDgvVisites()
+        {
+            dgvVisites.Rows.Clear();
+            if (dgvEchantillons.SelectedRows.Count == 0)
+            {
+                dgvVisites.Visible = false;
+            }
+            else
+            {
+                Medicament unMedicament = (Medicament)dgvEchantillons.SelectedRows[0].Cells[1].Value;
+                List<Visite> lesVisitesCloses = Globale.LeVisiteur.getLesVisitesCloses();
+
+                foreach (Visite uneVisite in lesVisitesCloses)
+                {
+                    // Dictionnaire contenant la liste des échantillons de chaque visites
+                    SortedDictionary<Medicament, int> lesEchantillons = uneVisite.getLesEchantillons();
+
+                    foreach (KeyValuePair<Medicament, int> unEchantillon in lesEchantillons)
+                    {
+                        if (unEchantillon.Key == unMedicament)
+                        {
+                            dgvVisites.Rows.Add(
+                                uneVisite,
+                                uneVisite.DateEtHeure.ToLongDateString() + " à " + uneVisite.DateEtHeure.ToShortTimeString(),
+                                uneVisite.LePraticien.NomPrenom,
+                                unEchantillon.Value
+                            );
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
